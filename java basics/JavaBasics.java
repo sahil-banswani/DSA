86a0c93dcdb8c5ad82bc53bbfb9b18448e8394ee
Sahil Banswani <sahilbanswani03@gmail.com>
2023-08-19 15:52:56 +0530

    Update JavaBasics.java

--- java basics/JavaBasics.java
+++ java basics/JavaBasics.java
@@ -1,20 +1,36 @@
 // import java.util.*;
 
 public class JavaBasics {
-    public static void main(String args[]) {   
-        System.out.println(multiply(3, 4));
-    } 
+    public static void main(String args[]) {
+        // System.out.print();
+        swap();
+    }
+
     static void pattern1(int n) {
-        for(int row = 0 ;row <= n; row++) {
-            //for every row, run the col
-            for(int col =0 ; col < row; col++) {
+        for (int row = 0; row <= n; row++) {
+            // for every row, run the col
+            for (int col = 0; col < row; col++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
-    public static int multiply (int n, int m) {
-        int product = n*m;
+
+    public static int multiply(int n, int m) {
+        int product = n * m;
         return product;
+    }
+
+    static void swap() {
+        int a = 5;
+        int b = 10;
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
     }
 }
