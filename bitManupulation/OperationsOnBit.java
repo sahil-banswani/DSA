b5ba34cda413fb60033e8c6a32bb0e7e77035e7e
Sahil Banswani <sahilbanswani03@gmail.com>
2024-04-22 18:08:31 +0530

    commit

--- bitManupulation/OperationsOnBit.java
+++ bitManupulation/OperationsOnBit.java
@@ -1,5 +1,19 @@
 class OperationsOnBit {
     public static void main(String[] arg) {
-        
+        System.out.println(setIthBit(10,2));
+    }
+
+    static int getIthBit (int n, int i) {
+        int bitmask = 1 << i;
+        if((n & bitmask) == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    static int setIthBit (int n, int i) {
+        int bitMask = n << i;
+        return n | bitMask;
     }
 }
