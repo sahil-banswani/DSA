d7618c2707b7c264b30a6dac3c8ceb6cba63873b
Sahil Banswani <sahilbanswani03@gmail.com>
2024-04-24 15:18:22 +0530

    commit

---
+++ Oops/student.java
@@ -0,0 +1,15 @@
+class student {
+    public static void main(String arg[]) {
+        Students s1 = new Students();
+    }
+}
+
+class Students {
+
+    String name;
+    int roll;
+
+    Students() {
+        // System.out.println();
+    };
+}
