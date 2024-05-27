f1ffcf003babc960f77d384b7da870415aa8b435
Sahil Banswani <sahilbanswani03@gmail.com>
2024-05-27 15:18:02 +0530

    commit

---
+++ Recurssion/RecurssionBasics.java
@@ -0,0 +1,38 @@
+
+public class RecurssionBasics {
+
+    public static void printDec(int n) {
+        if(n == 1) {
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n + " ");
+        printDec(n-1);
+    }
+
+    public static void printAsc(int n) {
+        if(n == 1) {
+            System.out.print(n);
+            return;
+        }
+        printAsc(n-1);
+        System.out.print(" " + n );
+    }
+
+    public static int Factorial(int n) {
+        if(n==0) {
+            return 1;
+        }
+        int fac1 = Factorial(n-1);
+        int fn = n * Factorial(n-1);
+        return fn;
+    }
+
+    public static void main(String arg[]) {
+        int n = 5;
+        // printDec(n);
+        // printAsc(n);
+
+        System.out.println(Factorial(n));
+    }
+}
