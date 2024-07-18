4c17506609e6639528be15a1002f995b5f2f853e
Sahil Banswani <sahilbanswani03@gmail.com>
2024-07-18 15:41:47 +0530

    commit

---
+++ arrayList/Classroom.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.utils.arraylist;
+
+public class Classroom {
+    public static void main(String[] arg) {
+        ArrayList<Integer> list = new ArrayList();
+    }
+}
