2bc3220210af6a47eabfa10b90314a4ca0f0f31a
Sahil Banswani <sahilbanswani03@gmail.com>
2024-07-18 17:03:33 +0530

    commit

--- arrayList/Classroom.java
+++ arrayList/Classroom.java
@@ -1,20 +1,32 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Classroom {
+
+    public static void swap(ArrayList<Integer> list, int idx1,int idx2) {
+        int temp = list.get(idx1);
+        list.set(idx1, list.get(idx2));
+        list.set(idx2, temp);         
+        
+    }
     public static void main(String[] arg) {
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
+        list.add(10);
+        list.add(95);
+        list.add(12);
+        list.add(85);
+        list.add(56);
 
-        // System.out.println(list);
+        // swap(list, 1, 3);
+
+        Collections.sort(list);
+        Collections.
+        System.out.println(list);
 
         // reverse Arraylist
-        for(int i = list.size()-1; i>=0; i--) {
-            System.out.print(list.get(i) + " ");
-        } 
+        // for(int i = list.size()-1; i>=0; i--) {
+        //     System.out.print(list.get(i) + " ");
+        // } 
 
         // Add element
         // list.add(1, 9); // O(n)
@@ -33,6 +45,9 @@ public class Classroom {
         // // Contains element - O(n)
         // System.out.println(list.contains(3));
         // System.out.println(list.contains(11));
+
+        // swap elements
+        
 
 
     }
