99e53773dc3f6d63daf5069998bbbe4d44293569
Sahil Banswani <sahilbanswani03@gmail.com>
2024-07-27 02:35:25 +0530

    commit

--- Divide&Conquer/MergeSort.java
+++ Divide&Conquer/MergeSort.java
@@ -2,12 +2,15 @@
 public class MergeSort {
     public static void printArr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + "");
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
 
     public static void mergeSort(int arr[], int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
         int mid = si + (ei - si) / 2;
         mergeSort(arr, si, mid);
         mergeSort(arr, mid + 1, ei);
@@ -17,21 +20,21 @@ public class MergeSort {
     public static void merge(int arr[], int si, int mid, int ei) {
 
         int[] temp = new int[ei - si + 1];
-        int i = si; // iterator fot left part
-        int j = mid + 1; // iterator for temp array
+        int i = si; // iterator fot left part array
+        int j = mid + 1; // iterator for right array
         int k = 0; // iterator for temp arr
 
-        while (i <= mid && j < ei) {
+        while (i <= mid && j <= ei) {
             if (arr[i] < arr[j]) {
                 temp[k] = arr[i];
                 i++;
             } else {
                 temp[k] = arr[j];
                 j++;
-
             }
             k++;
         }
+        // it may be posiible that one of the array is not completed
         while (i <= mid) {
             temp[k++] = arr[i++];
         }
@@ -46,8 +49,8 @@ public class MergeSort {
     }
 
     public static void main(String[] args) {
-        int arr[] = {6, 3, 9, 5, 2, 8};
-        mergeSort(arr, 0, arr.length-1);
+        int arr[] = { 6, 3, 9, 5, 2, 8, -2 };
+        mergeSort(arr, 0, arr.length - 1);
         printArr(arr);
     }
 }
